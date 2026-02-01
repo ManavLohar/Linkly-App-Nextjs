55fdda015887c1ea69e6a7759fb5636dee45267b
Manav Lohar <manavlohar414141@gmail.com>
2026-01-31 23:46:26 -0800

    Adding ui components, and custom hooks for video calling functionality.
    Signed-off-by: Manav Lohar <manavlohar414141@gmail.com>

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn, SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+  return (
+    <main>
+      <SignUp />
+    </main>
+  );
+};
+
+export default SignUpPage;
---
+++ components/CallList.tsx
@@ -0,0 +1,120 @@
+// @ts-nocheck
+"use client";
+
+import { useGetCalls } from "@/hooks/useGetCalls";
+import { Call, CallRecording } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import MeetingCards from "./MeetingCards";
+import Loader from "./Loader";
+import { toast } from "sonner";
+
+const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+    useGetCalls();
+  const router = useRouter();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+  const getCalls = () => {
+    switch (type) {
+      case "ended":
+        return endedCalls;
+        break;
+      case "recordings":
+        return recordings;
+        break;
+      case "upcoming":
+        return upcomingCalls;
+        break;
+      default:
+        return [];
+        break;
+    }
+  };
+
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case "ended":
+        return "No Previous Calls";
+        break;
+      case "recordings":
+        return "No Recordings";
+        break;
+      case "upcoming":
+        return "No Upcoming Calls";
+        break;
+      default:
+        return [];
+        break;
+    }
+  };
+
+  useEffect(() => {
+    const fetchRecordings = async () => {
+      try {
+        const callData = await Promise.all(
+          callRecordings.map((meeting) => meeting.queryRecordings()),
+        );
+
+        const recordings = callData
+          .filter((call) => call.recordings.length > 0)
+          .flatMap((call) => call.recordings);
+
+        setRecordings(recordings);
+      } catch (error) {
+        toast("Try again later!");
+      }
+    };
+    if (type === "recordings") fetchRecordings();
+  }, [type, callRecordings]);
+
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+      {calls && calls.length > 0 ? (
+        calls.map((meeting: Call | CallRecording, index) => (
+          <MeetingCards
+            key={(meeting as Call).id}
+            title={
+              (meeting as Call).state?.custom?.description?.substring(0, 26) ||
+              meeting?.filename?.substring(0, 26) ||
+              "Personal Meeting"
+            }
+            date={
+              meeting.state?.startsAt.toLocaleString() ||
+              meeting.start_time.toLocaleString()
+            }
+            icon={
+              type === "ended"
+                ? "/icons/previous.svg"
+                : type === "upcoming"
+                  ? "/icons/upcoming.svg"
+                  : "/icons/recordings.svg"
+            }
+            isPreviousMeeting={type === "ended"}
+            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
+            buttonText={type === "recordings" ? "Play" : "Start"}
+            handleClick={
+              type === "recordings"
+                ? () => router.push(`${meeting.url}`)
+                : () => router.push(`/meeting/${meeting.id}`)
+            }
+            link={
+              type === "recordings"
+                ? meeting.url
+                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`
+            }
+          />
+        ))
+      ) : (
+        <h1>{noCallsMessage}</h1>
+      )}
+    </div>
+  );
+};
+
+export default CallList;
